d8d40aabc667d93c91c981d21b4b1cdaba0737b4
Jeferson Martins <jeferson@germantech.com.br>
2023-10-18 16:38:33 -0300

    Mudando mais umas coisas

--- html/main/js/pages/dashboard.js
+++ html/main/js/pages/dashboard.js
@@ -11,8 +11,8 @@ $(function () {
 		$('.donut').peity('donut');
 	
 		// revenue-analysis           
-		 var revenueanalysis = jQuery('#revenueanalysis')
-			if (revenueanalysis.length > 0) {
+		 var mensal2 = jQuery('#mensal2')
+			if (mensal2.length > 0) {
 			   var options = {
 					chart: {
 						height: 359,
@@ -27,7 +27,7 @@ $(function () {
 					legend: {
 						show: false,
 					},
-					colors:['#2a93d5', '#FC3468'],
+					colors:['#FC3468'],
 					fill: {
 					type: 'gradient',
 						gradient: {
@@ -45,11 +45,7 @@ $(function () {
 					},
 					series: [{
 						name: 'Total Entradas',
-						data: [20, 30, 42, 53, 32, 50, 60, 43, 43, 32, 20, 40]
-					},
-					{
-						name: 'Total Saídas',
-						data: [30, 40, 29, 33, 42, 30, 40, 52, 33, 22, 40, 50]
+						data: [143, 109, 320, 103, 150, 240, 160, 443, 93, 90, 120, 140]
 					}],
 					tooltip: {
 						x: {
@@ -85,15 +81,92 @@ $(function () {
 					},
 				}
 			var chart = new ApexCharts(
-				document.querySelector("#revenueanalysis"),
+				document.querySelector("#mensal2"),
 				options
 			);
 			chart.render();
 		 }
 
+// horarios_mais_vendas           
+var horarios_vendas = jQuery('#horarios_vendas')
+if (horarios_vendas.length > 0) {
+   var options = {
+		chart: {
+			height: 359,
+			type: 'area',
+			toolbar: {
+			show: false
+			},
+		},
+		dataLabels: {
+			enabled: false
+		},
+		legend: {
+			show: false,
+		},
+		colors:['#2a93d5'],
+		fill: {
+		type: 'gradient',
+			gradient: {
+			  type: "vertical",
+			  shadeIntensity: 0,
+			  opacityFrom: 0.3,
+			  opacityTo: 0.2,
+			  gradientToColors: ['#fcc525'],
+			  stops: [0, 90, 100]
+			}
+	  },
+		stroke: {
+			curve: 'smooth',
+			width: 2
+		},
+		series: [{
+			name: 'Total Entradas',
+			data: [20, 30, 42, 53, 32, 50, 60, 43, 43, 32, 20, 40]
+		}],
+		tooltip: {
+			x: {
+				format: 'dd/MM/yy HH:mm'
+			},
+		},
+		xaxis: {
+			show: true,
+			labels: {
+				show: true,
+			},
+			 axisBorder: {
+				 color: '#cccccc',
+				 show: true,
+			 },
+			 axisTicks: {
+			 color: '#cccccc',
+			 },
+			categories: ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00'],
+		},
+		yaxis: {
+		labels: {
+			show: true,
+		},
+		axisBorder: {
+			 color: '#fff',
+			 show: true,
+		 },
+		},
+		grid: {
+		show: true,
+		borderColor: '#efefef',
+		},
+	}
+var chart = new ApexCharts(
+	document.querySelector("#horarios_vendas"),
+	options
+);
+chart.render();
+}
+
 		 // anual           
-		 var anual = jQuery('#anual')
-			if (anual.length > 0) {
+		 var anual2 = jQuery('#anual2')
+			if (anual2.length > 0) {
 			   var options = {
 					chart: {
 						height: 359,
@@ -108,7 +181,7 @@ $(function () {
 					legend: {
 						show: false,
 					},
-					colors:['#2a93d5', '#FC3468'],
+					colors:['#FC3468'],
 					fill: {
 					type: 'gradient',
 						gradient: {
@@ -125,12 +198,8 @@ $(function () {
 						width: 2
 					},
 					series: [{
-						name: 'Total Entradas',
-						data: [20, 30, 42, 53, 32, 50, 60, 43, 43, 32, 20, 40]
-					},
-					{
 						name: 'Total Saídas',
-						data: [30, 40, 29, 33, 42, 30, 40, 52, 33, 22, 40, 50]
+						data: [30, 40, 59, 93, 180, 200, 320, 402, 440, 520, 600, 300]
 					}],
 					tooltip: {
 						x: {
@@ -166,7 +235,7 @@ $(function () {
 					},
 				}
 			var chart = new ApexCharts(
-				document.querySelector("#anual"),
+				document.querySelector("#anual2"),
 				options
 			);
 			chart.render();
@@ -270,80 +339,98 @@ $(function () {
 		new ApexCharts(document.querySelector("#spark1"), spark1).render();
 	
 		
-	
 		var optionsBar = {
-		  chart: {
-			type: 'bar',
-			height: 359,
-			width: '100%',
-			stacked: true,
-			foreColor: '#999'
-		  },
-		  plotOptions: {
-			bar: {
-			  dataLabels: {
-				enabled: false
-			  },
-			  columnWidth: '75%',
-			  endingShape: 'rounded'
-			}
-		  },
-		  colors: ["#00C5A4", '#F3F2FC'],
-		  series: [{
-			name: "Total Entradas",
-			data: [20, 16, 24, 28, 26, 22, 15],
-		  }, {
-			name: "Total Saídas",
-			data: [18, 11, 14, 27, 21, 18, 10],
-		  }],
-		  xaxis: {
-			axisBorder: {
-			  show: false
+			chart: {
+			  type: 'bar',
+			  height: 359,
+			  width: '100%',
+			  foreColor: '#999'
 			},
-			categories: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'],
-			axisTicks: {
-			  show: false
-			},
-			crosshairs: {
-			  show: false
-			},
-			labels: {
-			  show: true,
-			  style: {
-				fontSize: '14px'
+			plotOptions: {
+			  bar: {
+				columnWidth: '75%',
+				endingShape: 'flat'
 			  }
 			},
-		  },
-		  grid: {
+			dataLabels: {
+			  enabled: true,
+			  style: {
+				fontSize: '14px', // Diminuir o tamanho da fonte
+				colors: ["#696969"] // Tornar os rótulos cinza
+			  },
+			  //formatter: function (val) {
+				//return "R$" + val.toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+			  //},
+			  offsetY: 20 // Mova o rótulo para o topo das barras
+			},
+			colors: ["#00C5A4"],
+			series: [{
+			  name: "Total Saídas",
+			  data: [180, 112, 140, 27, 21, 180, 100],
+			}],
 			xaxis: {
-			  lines: {
+			  axisBorder: {
 				show: false
 			  },
+			  categories: getWeekDaysWithDates(),
+			  axisTicks: {
+				show: false
+			  },
+			  crosshairs: {
+				show: false
+			  },
+			  labels: {
+				show: true,
+				style: {
+				  fontSize: '14px'
+				}
+			  },
+			},
+			grid: {
+			  xaxis: {
+				lines: {
+				  show: false
+				},
+			  },
+			  yaxis: {
+				lines: {
+				  show: false
+				},
+			  }
 			},
 			yaxis: {
-			  lines: {
+			  axisBorder: {
 				show: false
 			  },
+			  labels: {
+				show: false
+			  },
+			},
+			tooltip: {
+			  shared: true,
+			  y: {
+				formatter: function (val) {
+				  return "R$" + val.toLocaleString('pt-BR', { minimumFractionDigits: 2 });
+				}
+			  }
 			}
-		  },
-		  yaxis: {
-			axisBorder: {
-			  show: false
-			},
-			labels: {
-			  show: false
-			},
-		  },
-		  tooltip: {
-			shared: true
 		  }
-
-		}
-
-		var chartBar = new ApexCharts(document.querySelector('#bar'), optionsBar);
-		chartBar.render();
-	
-		WeatherIcon.add('icon1'	, WeatherIcon.SLEET , {stroke:false , shadow:false , animated:true } );
+		  
+		  function getWeekDaysWithDates() {
+			var days = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'];
+			var today = new Date();
+			var weekDaysWithDates = [];
+			for (var i = 6; i >= 0; i--) {
+			  var date = new Date(today);
+			  date.setDate(today.getDate() - i);
+			  var formattedDate = date.getDate() + '/' + (date.getMonth() + 1);
+			  weekDaysWithDates.push(days[(date.getDay())] + ' ' + formattedDate);
+			}
+			return weekDaysWithDates;
+		  }
+		  
+		  var chartBar = new ApexCharts(document.querySelector('#bar'), optionsBar);
+		  chartBar.render();
 	
 		
 	
