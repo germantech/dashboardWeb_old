68440c6a34782c6f17445518faef7ea3f381527a
Jeferson Martins <jeferson@germantech.com.br>
2023-10-20 14:09:20 -0300

    Adicionando uma parte no estoque

---
+++ html/bs-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    server: './', // Indica o diretório a ser servido pelo http-server
+    port: '8080',
+    files: ['*.html', 'css/**/*.css', 'js/**/*.js'], // Arquivos a serem observados para atualizações em tempo real
+  };
